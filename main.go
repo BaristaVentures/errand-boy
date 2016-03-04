d2d28a51064d1a8c188b61b56f5db15b4020e1b5
David Castillo <castillobgr@gmail.com>
2016-03-04 16:39:36 -0500

    Specify port via command line args.

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/BaristaVentures/errand-boy/server"
 )
 
 func main() {
-	s := server.Server{Port: 8080}
+	port := flag.Int("p", 8080, "The port where Errand Boy will run.")
+	flag.Parse()
+	s := server.Server{Port: *port}
 	fmt.Printf("Errand Boy is listening for your commands on port %d.\n", s.Port)
 	s.BootUp()
 }
