e448bb8c3aea21b0e4b4cd51cb5fe479ae216548
David Castillo <castillobgr@gmail.com>
2016-03-11 11:34:08 -0500

    Progress.

--- routers/tracker/router.go
+++ routers/tracker/router.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/BaristaVentures/errand-boy/routers"
+	"github.com/google/go-github/github"
 	"github.com/gorilla/mux"
 )
 
@@ -27,5 +28,10 @@ func Route(router *mux.Router) *mux.Router {
 func activityHandler(w http.ResponseWriter, r *http.Request) {
 	activity := &ActivityPayload{}
 	json.NewDecoder(r.Body).Decode(&activity)
+	// TODO: How to establish a relationship between te accepted story and the PR where the comment
+	// should be made? Obvious option is getting the list of open PRs and checking which one has the
+	// story's id in its title. Cons: the roundtrip can be expensive.
+	// TODO: How to know which repo is the one for the PT project? Should they be named the same?
+	client := github.NewClient(nil)
 	w.WriteHeader(http.StatusOK)
 }
