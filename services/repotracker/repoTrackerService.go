d79514dcf0d16e2e638a2b618ea5fc63ea929ec7
David Castillo <castillobgr@gmail.com>
2016-03-18 14:20:48 -0500

    Added support for reopened PRs.

--- services/repotracker/repoTrackerService.go
+++ services/repotracker/repoTrackerService.go
@@ -1,11 +1,11 @@
 package repotracker
 
 import (
-	"errors"
-
 	"github.com/BaristaVentures/errand-boy/routers/repos"
+	"github.com/BaristaVentures/errand-boy/services/logging"
 	"github.com/BaristaVentures/errand-boy/services/tracker"
 	"github.com/BaristaVentures/errand-boy/utils"
+	"github.com/Sirupsen/logrus"
 )
 
 var trackerService tracker.Service
@@ -22,19 +22,25 @@ func SetTrackerService(service tracker.Service) {
 var pullRequestHandler utils.ObserverFunc = func(payload interface{}) error {
 	prPayload := payload.(repos.PullRequest)
 	switch prPayload.Status {
+	case "reopened":
+		fallthrough
 	case "opened":
 		projectID, storyID, err := getTrackerData(&prPayload)
+		logrus.Info(projectID, storyID)
 		if err != nil {
+			logrus.Error(err)
 			return err
 		}
 		// Set the story as finished.
 		_, err = trackerService.SetStoryState(projectID, storyID, "finished")
 		if err != nil {
-			return errors.New("Request to Pivotal Tracker API (update story) Failed.")
+			logrus.Error(err)
+			return err
 		}
 
 		// Add a comment indicating the PR's URL.
 		trackerService.CommentOnStory(projectID, storyID, "Check the PR @ "+prPayload.URL)
+		logging.Info(&prPayload, "Sucessfully processed Pull Request:")
 	}
 	return nil
 }
